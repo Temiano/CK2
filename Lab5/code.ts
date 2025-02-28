60c18611103ab831e503913aeeb36cdf8920a263
Temiano <fosterartem776@gmail.com>
2025-02-28 11:41:12 +0400

    Add files via upload

---
+++ Lab5/code.ts
@@ -0,0 +1,27 @@
+let index = 0;
+const data: number[] = [12,23,34,45];
+console.log(data)
+let intervalDescriptor = setInterval(() => {
+        setTimeout(() => {
+            index++;    
+        }, 1);
+    if (index == data.length) {clearInterval(intervalDescriptor)}
+    if (data[index] % 2 == 1){
+        delete data[index];
+        console.log(data) 
+    } 
+}, 999);
+
+
+function makeFunc() {
+    var name = "Hello, World!";
+    
+    function displayName() {
+    console.log(name);
+    }
+    
+    return displayName;
+    };
+    
+var myFunc = makeFunc();
+myFunc();
