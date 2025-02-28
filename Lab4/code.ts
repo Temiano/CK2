8f1c64bc8c6528fac4bff01dcb71d2eb4ee9a97f
Temiano <fosterartem776@gmail.com>
2025-02-28 11:39:48 +0400

    Add files via upload

---
+++ Lab4/code.ts
@@ -0,0 +1,5 @@
+import { Transport } from "./Transport";
+
+console.log(1)
+let car = new Transport.Car("q","1",1234,123,123,Transport.Carcasses.CROSSOVER, Transport.CarClasses.A)
+console.log(car.info())
